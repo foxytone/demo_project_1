b414dfcb7d97daa2b5eaf21dab287d3c16ad788d
Neat0n <xcens@mail.ru>
2021-09-22 13:56:03 +0300

    added template and urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from authentication.views import login_page
 from django.urls import path, include
 
 # without lead-slash
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls'))
+    path('login/', login_page, name='login_page'),
 ]
